d430a75f5bf5eee537892c4aa585687e8c87fbce
khsrla9806 <kimhunsope@naver.com>
2023-04-04 18:11:55 +0900

    test: pr로 issue 닫아보기

--- src/main/java/com/fastcampus/Main.java
+++ src/main/java/com/fastcampus/Main.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         BubbleSort<String> sort = new BubbleSort<>();
+        // Pull Request로 issue 닫아보기
         System.out.println("[result] " + sort.sort(Arrays.asList(args)));
     }
 }
