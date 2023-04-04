0afca29d3143abfe0f7c3b04967a49dc881f45d1
khsrla9806 <kimhunsope@naver.com>
2023-04-04 22:31:41 +0900

    refactor: SortService를 Bean으로 등록
    생성자 주입에서 bubbleSort 빈을 주입받을 수 있도록 설정

--- src/main/java/com/fastcampus/springapply/service/SortService.java
+++ src/main/java/com/fastcampus/springapply/service/SortService.java
@@ -1,14 +1,17 @@
 package com.fastcampus.springapply.service;
 
 import com.fastcampus.springapply.logic.Sort;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SortService {
 
      private final Sort<String> sort;
 
-    public SortService(Sort<String> sort) {
+    public SortService(@Qualifier("bubbleSort") Sort<String> sort) {
         this.sort = sort;
     }
 
