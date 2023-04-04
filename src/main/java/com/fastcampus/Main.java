3ca3d8c18de6fc917cf8f2a729539ccbea5254ae
khsrla9806 <kimhunsope@naver.com>
2023-04-04 18:29:44 +0900

    Design #3 - create Sort Interface
    인터페이스를 설계하여, 각 구현체가 인터페이스를 구현하는 구조로 수정

--- src/main/java/com/fastcampus/Main.java
+++ src/main/java/com/fastcampus/Main.java
@@ -1,12 +1,14 @@
 package com.fastcampus;
 
 import com.fastcampus.logic.BubbleSort;
+import com.fastcampus.logic.JavaSort;
+import com.fastcampus.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
         // Pull Request로 issue 닫아보기
         System.out.println("[result] " + sort.sort(Arrays.asList(args)));
     }
