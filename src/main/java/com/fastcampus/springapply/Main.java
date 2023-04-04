61f606b04419e3d3148a45805c6e331340a5c596
khsrla9806 <kimhunsope@naver.com>
2023-04-04 22:29:29 +0900

    refactor: ApplicationContext를 사용해서 Bean을 가져올 수 있도록 설정

--- src/main/java/com/fastcampus/springapply/Main.java
+++ src/main/java/com/fastcampus/springapply/Main.java
@@ -1,7 +1,7 @@
 package com.fastcampus.springapply;
 
-import com.fastcampus.springapply.logic.JavaSort;
-import com.fastcampus.springapply.logic.Sort;
+import com.fastcampus.springapply.service.SortService;
+import com.fastcampus.springapply.config.Config;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,10 +10,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         // 어노테이션 기반의 컨텍스트를 생성
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        SortService sortService = context.getBean(SortService.class);
 
-        Sort<String> sort = new JavaSort<>();
-        // Pull Request로 issue 닫아보기
-        System.out.println("[result] " + sort.sort(Arrays.asList(args)));
+        System.out.println("[result] " + sortService.doSort(Arrays.asList(args)));
     }
 }
