c0b21eefbb7fbf969cb8b733d8a84c61bb2e1bb2
khsrla9806 <kimhunsope@naver.com>
2023-04-11 12:51:15 +0900

    feat: tomcat 서버 올리기 (war build)

--- src/main/java/com/fastcampus/springapply/config/Config.java
+++ src/main/java/com/fastcampus/springapply/config/Config.java
@@ -2,9 +2,7 @@ package com.fastcampus.springapply.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc // Web Mvc를 사용하기 위한 어노테이션
 @ComponentScan("com.fastcampus.springapply")
 @Configuration
 public class Config {
---
+++ src/test/java/com/fastcampus/springapply/service/SortServiceTest.java
@@ -0,0 +1,23 @@
+package com.fastcampus.springapply.service;
+
+import com.fastcampus.javaoop.logic.JavaSort;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SortServiceTest {
+
+    private com.fastcampus.javaoop.service.SortService sort = new com.fastcampus.javaoop.service.SortService(new JavaSort<>());
+    @Test
+    void doSort() {
+        // Given
+
+        // When
+        List<String> actual = sort.doSort(List.of("3", "2", "1"));
+
+        // Then
+        assertEquals(List.of("1", "2", "3"), actual);
+    }
+}
